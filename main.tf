c7e37d7b39fc52deaa2c68c07e8b068050a841cc
ranog <jprnogueira@yahoo.com.br>
2022-02-02 09:23:29 -0300

    add the code to read from the external data source

--- main.tf
+++ main.tf
@@ -3,8 +3,18 @@ provider "aws" {
   profile = "tf-user"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name    = "name"
+    values  = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "helloworld" {
-  ami             = "ami-09dd2e08d601bff67"
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   tags = {
       Name = "HelloWorld"
