1846694bb7a929a5c164345d508718301bd03876
ranog <jprnogueira@yahoo.com.br>
2022-03-02 13:43:07 -0300

    3.1.6: output values

--- chapter-03-functional-programming/madlibs.tf
+++ chapter-03-functional-programming/madlibs.tf
@@ -11,46 +11,41 @@ terraform {
 variable "words" {
   description = "A word pool to use for Mad Libs"
   type = object({
-      nouns         = list(string),
-      adjectives    = list(string),
-      verbs         = list(string),
-      adverbs       = list(string),
-      numbers       = list(number),
+      nouns       = list(string),
+      adjectives  = list(string),
+      verbs       = list(string),
+      adverbs     = list(string),
+      numbers     = list(number),
   })
-
-  validation {
-    condition     = length(var.words["nouns"]) >= 20
-    error_message  = "At least 20 nouns must be supplied."
-  }
 }
 
-resource "random_shuffle" "randow_nouns" {
+resource "random_shuffle" "random_nouns" {
   input = var.words["nouns"]
 }
 
-resource "random_shuffle" "randow_adjectives" {
+resource "random_shuffle" "random_adjectives" {
   input = var.words["adjectives"]
 }
 
-resource "random_shuffle" "randow_verbs" {
+resource "random_shuffle" "random_verbs" {
   input = var.words["verbs"]
 }
 
-resource "random_shuffle" "randow_adverbs" {
+resource "random_shuffle" "random_adverbs" {
   input = var.words["adverbs"]
 }
 
-resource "random_shuffle" "randow_numbers" {
+resource "random_shuffle" "random_numbers" {
   input = var.words["numbers"]
 }
 
-templatefile("${path.module}/templates/alice.txt",
-  {
-    nouns=random_shuffle.random_nouns.result
-    adjectives=random_shuffle.random_adjective.result
-    verbs=random_shuffle.random_verbs.result
-    adverbs=random_shuffle.random_adverbs.result
-    numbers=random_shuffle.random_numbers.result
-
-  }
-)
+output "mad_libs" {
+  value = templatefile("${path.module}/templates/alice.txt",
+    {
+      nouns       =random_shuffle.random_nouns.result
+      adjectives  =random_shuffle.random_adjectives.result
+      verbs       =random_shuffle.random_verbs.result
+      adverbs     =random_shuffle.random_adverbs.result
+      numbers     =random_shuffle.random_numbers.result
+  })
+}
