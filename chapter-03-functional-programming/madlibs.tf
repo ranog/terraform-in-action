ad1601d660d1c72f660197b35444a0b101c9eb76
ranog <jprnogueira@yahoo.com.br>
2022-03-09 19:40:35 -0300

    count parameter

--- chapter-03-functional-programming/madlibs.tf
+++ chapter-03-functional-programming/madlibs.tf
@@ -19,27 +19,37 @@ variable "words" {
   })
 }
 
+variable "num_files" {
+  default = 100
+  type = number
+}
+
 locals {
   uppercase_words = {for k, v in var.words : k => [for s in v : upper(s)]}
 }
 
 resource "random_shuffle" "random_nouns" {
+  count = var.num_files
   input = local.uppercase_words["nouns"]
 }
 
 resource "random_shuffle" "random_adjectives" {
+  count = var.num_files
   input = local.uppercase_words["adjectives"]
 }
 
 resource "random_shuffle" "random_verbs" {
+  count = var.num_files
   input = local.uppercase_words["verbs"]
 }
 
 resource "random_shuffle" "random_adverbs" {
+  count = var.num_files
   input = local.uppercase_words["adverbs"]
 }
 
 resource "random_shuffle" "random_numbers" {
+  count = var.num_files
   input = local.uppercase_words["numbers"]
 }
 
